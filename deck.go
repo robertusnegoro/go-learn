1c10f8b0062afde67d6a04dd494f28048a792867
Robertus Negoro <895404+robertusnegoro@users.noreply.github.com>
2023-07-10 18:23:04 +0700

    init

---
+++ deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// create new type of 'deck'
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	// for i, suit := range cardSuits { // i will not be used, so replace it with _ for every unused var
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
